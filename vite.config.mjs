bcfa338276106580a89cc4a5f9aae7401728497e
Ruohao Chen <edwardchan22@hotmail.com>
2025-07-26 11:12:13 +0900

    change to mjs

---
+++ vite.config.mjs
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { resolve, dirname } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist"
+  },
+  server: {
+    hmr: {
+      overlay: true
+    }
+  },
+  resolve: {
+    alias: {
+      "@langs": resolve(__dirname, "src/langs"),
+      "@": resolve(__dirname, "src")
+    }
+  }
+});
